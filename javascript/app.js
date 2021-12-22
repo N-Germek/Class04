59bdf959088bf859e87ec235722df6db7fcdd742
Natalija Germek <n.germek@icloud.com>
2021-12-22 02:21:10 -0800

    working?

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,8 @@
 let amount = prompt("How many bags would you like to purchase?")
 let message = ""
 
-if (amount < 1 && 2) {
-    message = "Fairweather Coffee fan? you can do better."
+if (amount < 2) {
+    message = "Fairweather Coffee fan? ...you can do better."
 }
 else {
     message = "A true Coffee Connoisseur."
