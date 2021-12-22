ca9b08f9db590c5b544080a6aca12dc39f3cf8f0
Natalija Germek <n.germek@icloud.com>
2021-12-22 02:11:54 -0800

    added JavaScript Page

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+let amount = prompt("How many bags would you like to purchase?")
+let message = ""
+
+if (amount < 1 && 2) {
+    message = "Fairweather Coffee fan? you can do better."
+}
+else {
+    message = "A true Coffee Connoisseur."
+}
