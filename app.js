655d49117f160a401bc7d4b81d0da2658aa003f0
Natalija Germek <n.germek@icloud.com>
2021-12-23 23:00:43 -0800

    hope this works on my JS

---
+++ app.js
@@ -0,0 +1,11 @@
+let amount = prompt("How many bags would you like to purchase?");
+let message = "";
+
+if (amount < 2) {
+    message = "Fairweather Coffee fan? ...you can do better.";
+}
+else {
+    message = "A true Coffee Connoisseur."; 
+}
+
+document.write(message);
